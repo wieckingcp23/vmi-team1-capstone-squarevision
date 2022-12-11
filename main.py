6c058a89472289009d9c345db354e1af058b1aad
wieckingcp23 <pwiecking@outlook.com>
2022-12-11 16:56:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def start():
     while True:
         success, img = cap.read()
         #confidence threashold = how sure the algo is that an object is an object
-        classIds, confs, bbox = net.detect(img, confThreshold = .5)
+        classIds, confs, bbox = net.detect(img, confThreshold = float(e1.get()))
         print(classIds, bbox)
 
         if len(classIds) != 0:
@@ -96,39 +96,37 @@ def change(val):
     return conf
 
 
+
 master = tkinter.Tk("Cap")
 master.title("Capstone Sprint 2")
 master.configure(bg='lightgrey')
 master.geometry('450x450')
 
+L1 = ttk.Label(master, text="Fall 2022 Pre-Capstone").grid(column=0, row=0)
+L2 = ttk.Label(master, text="Team 1").grid(column=0, row=1)
+
+
 #These are the start up buttons
-buttonCustom = ttk.Button(master, text='Start Program', command=lambda:start())
-buttonDefault = ttk.Button(master, text='Default Settings', command=lambda:default())
+buttonCustom = ttk.Button(master, text='Start Program', command=lambda:start()).grid(column = 1, row = 20)
+buttonDefault = ttk.Button(master, text='Default Settings', command=lambda:default()).grid(column = 1, row = 22)
+L3 = ttk.Label(master, text= "Run Custom Settings             ->").grid(column=0, row=20)
+L4 = ttk.Label(master, text= "Run Default Settings             ->").grid(column=0, row=22)
 
 #Camera variable selection buttons - working (somehow)
-internal = ttk.Button(master, text="Internal Camera", command=lambda:setVid(0))
-external = ttk.Button(master, text="External Camera", command=lambda:setVid(1))
+internal = ttk.Button(master, text="Internal Camera", command=lambda:setVid(0)).grid(column = 1, row = 10)
+external = ttk.Button(master, text="External Camera", command=lambda:setVid(1)).grid(column = 1, row = 11)
+L5 = ttk.Label(master, text= "Use Internal Camera             ->").grid(column=0, row=10)
+L6 = ttk.Label(master, text= "Use External Camera             ->").grid(column=0, row=11)
 
 #Confidence Threashold selection with slider (DID NOT WORK)
 #s1 = Scale(master, from_ = 1, to = 99, orient = HORIZONTAL, command=change)
 #s1.grid(column=1, row=2)
 #s1.grid(column = 10, row = 70)
 
+
 #Confidence Threashold selection with User input
-L3 = ttk.Label(master, text="Adjust Confidence Threashold").grid(column=1, row=1)
-num = ttk.Label(master, text="Enter a Number from 0.0 to 0.99 ").grid(column =1, row =2)
-e1 = ttk.Entry(master).grid(row=4, column=2)
-
-internal.grid(column = 1, row = 5)
-external.grid(column = 1, row = 5)
-
-buttonCustom.grid(column = 5, row = 30)
-buttonDefault.grid(column = 5, row = 30)
-
-L1 = ttk.Label(master, text="Fall 2022 Pre-Capstone").grid(column=5, row=0)
-
-L2 = ttk.Label(master, text="Team 1").grid(column=3, row=1)
-
-
+L7 = ttk.Label(master, text="Adjust Confidence Threashold").grid(column=0, row=2)
+num = ttk.Label(master, text="Enter a Number from 0.0 to 0.99 ").grid(column =1, row =1)
+e1 = ttk.Entry(master).grid(column=1, row=2)
 
 master.mainloop()
