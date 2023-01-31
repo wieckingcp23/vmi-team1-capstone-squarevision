36d1d6ef2b5b7ed45b95201138024191c7b2da20
wieckingcp23 <pwiecking@outlook.com>
2023-01-30 20:35:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ import tkinter as tk
 import cv2
 from tkinter import *
 from PIL import ImageTk, Image
+import time
 
-bg = PhotoImage(file = "vmi.png")
+
+def countdown(t):
+
+    while t:
+        time.sleep(1)
+        t -= 1
+
+def toInput():
+    global inputValue
+    inputValue=textbox.get("1.0","end-1c")
+    return inputValue
 
 
 #sets the value that determines internal or external camera
@@ -19,42 +30,39 @@ def change(val):
 
 #this is where we will let the user customize options
 def start():
-    cap = cv2.VideoCapture(vid)
-    cap.set(3, 640)
-    cap.set(4, 480)
 
-    classNames = []
-    classFile = 'coco.names'
-    with open(classFile, 'rt') as f:
-        classNames = f.read().rstrip('\n').split('\n')
+    while countdown(inputValue):
+        cap = cv2.VideoCapture(vid)
+        cap.set(3, 640)
+        cap.set(4, 480)
+        classNames = []
+        classFile = 'coco.names'
+        with open(classFile, 'rt') as f:
+            classNames = f.read().rstrip('\n').split('\n')
+        
+        configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+        weightsPath = 'frozen_inference_graph.pb'
+        
+        net = cv2.dnn_DetectionModel(weightsPath, configPath)
+        net.setInputSize(320, 320)
+        net.setInputScale(1.0 / 127.5)
+        net.setInputMean((127.5, 127.5, 127.5))
+        net.setInputSwapRB(True)
+        
+        while True:
+            success, img = cap.read()
+            classIds, confs, bbox = net.detect(img, confThreshold = x)
+            print(classIds, bbox)
+            
+            if len(classIds) != 0:
+                
+                for classIds, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+                    cv2.rectangle(img, box, color = (0, 255, 0), thickness = 2)
+                    cv2.putText(img, classNames[classIds-1].upper(), (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,255,0), 2)
+                    
+            cv2.imshow("Square Vision", img)
+            cv2.waitKey(1)
 
-    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-    weightsPath = 'frozen_inference_graph.pb'
-
-    net = cv2.dnn_DetectionModel(weightsPath, configPath)
-    net.setInputSize(320, 320)
-    net.setInputScale(1.0 / 127.5)
-    net.setInputMean((127.5, 127.5, 127.5))
-    net.setInputSwapRB(True)
-
-
-    while True:
-        success, img = cap.read()
-        #confidence threashold = how sure the algo is that an object is an object
-        classIds, confs, bbox = net.detect(img, confThreshold = x)
-        print(classIds, bbox)
-
-        if len(classIds) != 0:
-
-            for classIds, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-                cv2.rectangle(img, box, color = (0, 255, 0), thickness = 2)
-                cv2.putText(img, classNames[classIds-1].upper(), (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,255,0), 2)
-
-
-
-
-        cv2.imshow("Square Vision", img)
-        cv2.waitKey(1)
 
 # This is the orginial scrpit - do not change
 def default():
@@ -121,6 +129,9 @@ buttonFrame = tk.Frame(root)
 buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
 buttonFrame.columnconfigure(2, weight=1)
+buttonFrame.columnconfigure(3, weight=1)
+
+
 
 label4 = tk.Label(buttonFrame, text = "Use Internal Camera", font =('Arial', 13))
 label4.grid(row=0, column=0, sticky =tk.W+tk.E)
@@ -136,23 +147,28 @@ external.grid(row=1, column=1, sticky =tk.W+tk.E)
 
 label6 = tk.Label(buttonFrame, text="Adjust Confidence Threashold", font=('Arial', 13))
 label6.grid(row=2, column=0, sticky =tk.W+tk.E)
-
-
 s1 = Scale(buttonFrame, from_ = 1, to = 100, resolution =2, orient = HORIZONTAL, command=change)
-#s1.pack()
 s1.grid(row=2, column=1, sticky =tk.W+tk.E)
 
 
-label7 = tk.Label(buttonFrame)
-label7.grid(row=3, column=0,sticky =tk.W+tk.E)
-label8 = tk.Label(buttonFrame)
-label8.grid(row=3, column=2, sticky =tk.W+tk.E)
+lbl7 = tk.Label(buttonFrame, text = "Set timer", font=('Arial', 13))
+lbl7.grid(row=3, column=0,sticky =tk.W+tk.E)
+textbox = tk.Text(buttonFrame,height = .5, width = 1, font=('Arial', 13))
+textbox.grid(row=3, column=1,sticky =tk.W+tk.E)
+btnTxt = tk.Button(buttonFrame, text = "Enter", command=lambda: toInput())
+btnTxt.grid(row=3, column=2, sticky =tk.W+tk.E)
+
+
+label9 = tk.Label(buttonFrame)
+label9.grid(row=4, column=0,sticky =tk.W+tk.E)
+label10 = tk.Label(buttonFrame)
+label10.grid(row=5, column=2, sticky =tk.W+tk.E)
 
 custom = tk.Button(buttonFrame, text="Custom Settings", command=lambda:start(), font=('Arial', 14))
-custom.grid(row=4, column=0,sticky =tk.W+tk.E)
+custom.grid(row=6, column=0,sticky =tk.W+tk.E)
 
 custom2 = tk.Button(buttonFrame, text="Default Settings", command=lambda:default(), font=('Arial', 14))
-custom2.grid(row=4, column=2, sticky =tk.W+tk.E)
+custom2.grid(row=6, column=2, sticky =tk.W+tk.E)
 
 #buttonFrame.pack(fill = 'x')
 buttonFrame.pack()
